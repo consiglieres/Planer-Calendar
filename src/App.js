67b082915fd4f4ea700123c96015c84e111ff7a6
Городецкий Никита Витальевич <98966123+consiglieres@users.noreply.github.com>
2024-01-27 14:02:15 +0300

    Add new file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
+import Header from './components/calendar components/header.jsx'
 
 export default function App() {
   return (
     <div className="App">
-          <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+     
     </div>
   );
 };
--- src/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Header(){
-    return(
-        <header>
-            <h1>Привет Мир!!</h1>
-        </header>
-    )
-}
