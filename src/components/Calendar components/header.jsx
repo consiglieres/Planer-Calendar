b8502cd816315e3ca7a013368a596e8808b2d773
Городецкий Никита Витальевич <98966123+consiglieres@users.noreply.github.com>
2024-01-27 17:56:46 +0300

    add header

---
+++ src/components/Calendar components/header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Header(){
+    return(
+        <header>
+            <section id="left_header">
+                <img id="icon_calendar" src="../Sources/calendar_icon_151795.png"></img>
+                <div id="logo">Календарь</div>
+                <div></div>
+            </section>
+            <section id="right_header">
+                <div></div>
+                <div></div>
+                <div></div>
+                <img id="icon_user" src="../Sources/profile_user_icon_151793.png"></img>
+            </section>
+        </header>
+    )
+}
