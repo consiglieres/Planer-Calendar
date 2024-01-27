0655e1085c690c30925d386e6f9d9ad1dcd0ff7c
Городецкий Никита Витальевич <98966123+consiglieres@users.noreply.github.com>
2024-01-27 15:49:08 +0300

    Last

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-<<<<<<< Updated upstream
-import Header from './Header';
-=======
 import Calendar from './pages/calendar.jsx'
->>>>>>> Stashed changes
 
 export default function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
-          <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
       <Calendar />
-     
->>>>>>> Stashed changes
+
     </div>
   );
 };
