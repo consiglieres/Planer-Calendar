0bfcbcf1a4930ee0f01717086882674cd2180577
Городецкий Никита Витальевич <98966123+consiglieres@users.noreply.github.com>
2024-01-30 15:20:19 +0300

    New progect

--- src/components/calendar components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer(){
-    return(<footer>
-        <section id=""></section>
-        <section id=""></section>
-        <section id=""></section>
-    </footer>)
-}
